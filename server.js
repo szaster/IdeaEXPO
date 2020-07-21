d1bcf89ccd006951895b1c63e5a30196d7cffc92
Svitlana Zaster <szaster@protonmail.com>
2020-07-21 15:35:38 -0500

    Unsuccessful attempts to wire up Google Auth

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
+// Requiring handlebars
+const exphbs = require("express-handlebars");
+
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-const exphbs = require("express-handlebars");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -20,20 +22,20 @@ app.use(
 );
 //Passport middleware
 app.use(passport.initialize());
+// integrate passport with our express session middleware
 app.use(passport.session());
 
 // Handlebars
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Requiring our routes
-app.use("/", require("./controllers/index"));
-app.use("/auth", require("./controllers/auth"));
+// app.use("/", require("./controllers/index"));
+// app.use("/auth", require("./controllers/auth"));
 
-// require("./controllers/auth")(app);
+require("./controllers/auth")(app);
 // require("./controllers/route")(app);
-// require("./controllers/api-routes.js")(app);
+require("./controllers/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
