77cd047d3517c9931000e2ef8f726539cee1e1c3
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-07-20 21:11:52 -0500

    added middleware authentication &  get routes for intro and dashboard

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./controllers/html-routes.js")(app);
+require("./controllers/route")(app);
 require("./controllers/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
