4d6bdeb648703214952791cc529a16d3c31c42cf
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-07-21 19:59:33 -0500

    passport setup

--- config/passport.js
+++ config/passport.js
@@ -1,82 +1,95 @@
-// const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const user = require('../models/user')
 
-// const db = require("../models/user");
-// // const { Strategy } = require("passport");
-// // const user = require("../models/user");
-
-// // require("dotenv").config();
-
-// passport.use(
-//   new GoogleStrategy(
-//     {
+// module.exports = function(passport) {
+//   passport.use(new GoogleStrategy({
 //       clientID: process.env.GOOGLE_CLIENT_ID,
 //       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: "/auth/google/callback",
+//       callbackURL: '/auth/google/callback'
+//   },
+//   async (accessToken, refreshToken, profile, done) => {
+//       const newUser = {
+//           googleId: profile.id,
+//           displayName: profile.displayName,
+//           firstName: profile.name.givenName,
+//           lastName: profile.name.familyName,
+//           image: profile.photos[0].value
+//       }
+
+//       try {
+//           let user = await User.findOne({ googleId: profile.id })
+
+//           if(user) {
+//               done(null, user)
+//           } else {
+//               user = await User.create(newUser)
+//               done(null, user)
+//           }
+//       } catch (err) {
+//           console.error(err)
+//       }
+//   }))
+
+//   passport.serializeUser((user, done) => {
+//       done(null, user.id)
+//   })
+
+//   passport.deserializeUser((id, done) => {
+//       User.findById(id, (err, user) =>  done(err, user))
+//   })
+// }
+
+// passport.use(
+//   new LocalStrategy(
+//     // Our user will sign in using an email, rather than a "username"
+//     {
+//       usernameField: "email",
 //     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       console.log(profile);
+//     (email, password, done) => {
+//       // When a user tries to sign in this code runs
+//       db.User.findOne({
+//         where: {
+//           email: email,
+//         },
+//       }).then((dbUser) => {
+//         // If there's no user with the given email
+//         if (!dbUser) {
+//           return done(null, false, {
+//             message: "Incorrect email.",
+//           });
+//         }
+//         // If there is a user with the given email, but the password the user gives us is incorrect
+//         else if (!dbUser.validPassword(password)) {
+//           return done(null, false, {
+//             message: "Incorrect password.",
+//           });
+//         }
+//         // If none of the above, return the user
+//         return done(null, dbUser);
+//       });
 //     }
 //   )
 // );
 
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
+// // In order to help keep authentication state across HTTP requests,
+// // Sequelize needs to serialize and deserialize the user
+// // Just consider this part boilerplate needed to make it all work
+// passport.serializeUser((user, cb) => {
+//   cb(null, user);
 // });
 
-// passport.deserializeUser((id, done) => {
-//   user.findById(id, (err, user) => done(err, user));
+// passport.deserializeUser((obj, cb) => {
+//   cb(null, obj);
 // });
 
-// // // Exporting our configured passport
+// // Exporting our configured passport
 // module.exports = passport;
-
-/////////////////*******OLD Code below********//////////////////////////
-// Telling passport we want to use a Local Strategy.
-//In other words, we want login with a username/email and password
 passport.use(
-  new LocalStrategy(
-    // Our user will sign in using an email, rather than a "username"
-    {
-      usernameField: "email",
-    },
-    (email, password, done) => {
-      // When a user tries to sign in this code runs
-      db.User.findOne({
-        where: {
-          email: email,
-        },
-      }).then((dbUser) => {
-        // If there's no user with the given email
-        if (!dbUser) {
-          return done(null, false, {
-            message: "Incorrect email.",
-          });
-        }
-        // If there is a user with the given email, but the password the user gives us is incorrect
-        else if (!dbUser.validPassword(password)) {
-          return done(null, false, {
-            message: "Incorrect password.",
-          });
-        }
-        // If none of the above, return the user
-        return done(null, dbUser);
-      });
-    }
-  )
-);
-
-// In order to help keep authentication state across HTTP requests,
-// Sequelize needs to serialize and deserialize the user
-// Just consider this part boilerplate needed to make it all work
-passport.serializeUser((user, cb) => {
-  cb(null, user);
-});
-
-passport.deserializeUser((obj, cb) => {
-  cb(null, obj);
-});
-
-// Exporting our configured passport
-module.exports = passport;
+  new GoogleStrategy({
+    // options for the strat
+  }), () => {
+// callback function
+  }
+)
--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
-// const dotenv = require("dotenv");
-
-// dotenv.config({ path: "./config/config.env" });
-
-// Requiring passport as we've configured it
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const app = express();
 const passport = require("./config/passport");
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+// Passport config
+//require("./config/passport")(passport);
+// Requiring passport as we've configured it
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+	session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
+
+// Handlebars
+app.engine(".hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  // Add Sequelize here
+  //store: new MongoStore({ mongooseConnection: mongoose.connection})
+}))
+
 //Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Handlebars
-const exphbs = require("express-handlebars");
-
-app.engine(".hbs", exphbs({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
 
 // Requiring our routes
 // app.use("/", require("./controllers/index"));
@@ -41,11 +54,11 @@ require("./controllers/index")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+	app.listen(PORT, () => {
+		console.log(
+			"==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+			PORT,
+			PORT
+		);
+	});
 });
