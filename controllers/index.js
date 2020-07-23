e7f98ec93890c89e9a3d63b2d587444472d14247
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-07-23 00:53:46 -0500

    added server 500 error, idea form

--- controllers/index.js
+++ controllers/index.js
@@ -5,46 +5,45 @@ const router = express.Router();
 
 // Requiring our custom middleware for checking if a user is logged in
 const {
-  ensureAuth,
-  ensureUser,
+	ensureAuth,
+	ensureUser,
 } = require("../config/middleware/isAuthenticated");
 
-const idea = require("../models/idea");
+const Idea = require("../models");
 
 //   Login/Landing page
 //   GET /
 router.get("/", (req, res) => {
-  res.render("intro", {
-    layout: "intro",
-  });
+	res.render("intro", {
+		layout: "intro",
+	});
 });
 //   Dashboard
 //   GET /dashboard
+// router.get("/dashboard", ensureAuth, (req, res) => {
+//     Idea.Post.findOne({
+//       where: {
+// 		name: req.user.firstName,
+// 		ideas,
+//       }
+//     })
+//       .then( (dbPost) => {
+//         res.json(dbPost);
+//       });
+//   });
 
-//////////old code below//////////
-
-router.get("/dashboard", ensureAuth, (req, res) => {
-  // If the user already has an account send them to the home page
-  res.render("dashboard", {
-    layout: "main",
-  });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+	try {
+		// await
+		const ideas = await Idea.findAll({ user: req.user.id });
+		res.render("dashboard", {
+			name: req.user.firstName,
+			ideas,
+		});
+	} catch (err) {
+		console.error(err);
+		res.render("error/500");
+	}
 });
 
 module.exports = router;
-
-// router.get("/dashboard", ensureAuth, async (req, res) => {
-//   // If the user already has an account send them to the home page
-//   try {
-//     const ideas = await idea.findOne({ where: { user: req.user.id } }).lean();
-//     res.render("dashboard", {
-//       name: req.user.firstName,
-//       ideas,
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     res.render("error/500");
-//   }
-//   // res.render("dashboard", {
-//   //   layout: "main",
-//   // });
-// });
