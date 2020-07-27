0720df8732ece6cd036132750d70036a1a60b72a
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-07-26 20:14:06 -0500

    added a connection  file

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        database: "ideas_db",
+        user: "root",
+        port: 3306,
+        password: "rootroot"
+    });
+}
+
+module.exports = connection;
